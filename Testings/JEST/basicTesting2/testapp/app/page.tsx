b08bb8b80b08da638734e66e8649fa3e6b7367b7
Shadabdullah <Shadabdullah101@gmail.com>
2024-02-18 22:04:03 +0530

    Setup Next Test

---
+++ Testings/JEST/basicTesting2/testapp/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-white flex justify-center items-center">
+
+       <h1 className="text-black text-4xl font-mono font-bold">Main page</h1>
+    </div>
+    
+  );
+}
