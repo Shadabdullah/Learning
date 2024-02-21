f7b33c414bd901bcb62a288c0cb200cddc7f29f5
Shadabdullah <Shadabdullah101@gmail.com>
2024-02-21 22:11:10 +0530

    Variables

--- Flutter/Dart/main.dart
+++ Flutter/Dart/main.dart
@@ -1,5 +1,21 @@
-
-void main(){
-
-   print("Running") ;
+void main() {
+  // Variables
+  print(variableFun());
 }
+
+String variableFun() {
+  dynamic useAnyDataType = "string";
+  print(useAnyDataType);
+
+  const valueA = 7;
+  const valueB = 2 * valueA;
+  print(valueB);
+
+  var buffer = StringBuffer() ;
+
+  for (var i = 0 ; i < 100 ; i++) {
+    buffer.write('$i');
+    print(buffer);
+  }
+  var value = buffer.toString();
+  return "VariableFun"; }
