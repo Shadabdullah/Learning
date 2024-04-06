9a95763ae338d579afd4707e744330ad1829677c
Shadabdullah <shadabdullah101@gmail.com>
2024-04-06 21:54:57 +0530

    Backend

---
+++ Flutter/data/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          // useMaterial3: true, // Uncomment this line if you're using Flutter 3.0
+          ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[100],
+        title: const Text("Data & Backend UI"),
+      ),
+    );
+  }
+}
