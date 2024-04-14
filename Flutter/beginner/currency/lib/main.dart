af10e3b39284a12ae97dde3b79509d47fbf6efab
Shadabdullah <shadabdullah101@gmail.com>
2024-04-14 13:18:27 +0530

    CurrencyAPp

--- Flutter/beginner/currency/lib/main.dart
+++ Flutter/beginner/currency/lib/main.dart
@@ -5,28 +5,95 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          background: const Color(0xFF607274), // Set the background color here
+        ),
+      ),
       home: const CurrencyApp(),
     );
   }
 }
 
-class CurrencyApp extends StatelessWidget {
-  const CurrencyApp({super.key});
+class CurrencyApp extends StatefulWidget {
+  const CurrencyApp({Key? key}) : super(key: key);
 
   @override
+  State<CurrencyApp> createState() => _CurrencyAppState();
+}
+
+class _CurrencyAppState extends State<CurrencyApp> {
+  final TextEditingController texteditingController = TextEditingController();
+
+  double result = 0;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('CurrencyApp'),
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
       ),
-      body: const Center(
-        child: Text('InCenter'),
+      borderRadius: BorderRadius.circular(5),
+    );
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'INR $result',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 33,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: texteditingController,
+                  style: const TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    hintText: "Please enter amount in USD",
+                    hintStyle: const TextStyle(color: Colors.black),
+                    prefixIcon: const Icon(Icons.monetization_on_outlined,
+                        color: Colors.black), // Adjusted icon color
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: border,
+                    focusedBorder: border,
+                  ),
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                    onPressed: () {
+                      result =
+                          double.tryParse(texteditingController.text)! * 84;
+                      print(result);
+                    },
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        minimumSize: const Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5))),
+                    child: const Text(
+                      'Convert',
+                    ))
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
