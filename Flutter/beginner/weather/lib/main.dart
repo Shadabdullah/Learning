10204ee9c88694c8efcf316d69608b7ef18bb2e6
Shadabdullah <shadabdullah101@gmail.com>
2024-04-18 14:31:42 +0530

    Main Screen

--- Flutter/beginner/weather/lib/main.dart
+++ Flutter/beginner/weather/lib/main.dart
@@ -13,8 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        theme: ThemeData.dark(
           useMaterial3: true,
         ),
         home: const MyHomePage());
@@ -28,8 +27,44 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Weather"),
+        centerTitle: true,
+        title: const Text(
+          "Weather App",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.refresh,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: const [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                child: Column(
+                  children: [
+                    Text('300.75 F'),
+                    SizedBox(height: 20),
+                    Icon(
+                      Icons.cloud,
+                    ),
+                    SizedBox(height: 20),
+                    Text('Rain')
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
